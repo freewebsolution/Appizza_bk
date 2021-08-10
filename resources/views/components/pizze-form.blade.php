daca3aa10e0b7b6dbeea4fc6059180f34031d8f0
lucio Ticali <info@freewebsolution.it>
2021-08-10 19:14:17 +0200

    Blade, Form , Create e store

--- resources/views/components/pizze-form.blade.php
+++ resources/views/components/pizze-form.blade.php
@@ -1,4 +1,4 @@
-@props(['user'])
+@props(['pizza'])
 <form {{ $attributes->merge() }}>
     {{ $slot }}
     <div class="border-b">
@@ -8,14 +8,17 @@
                     <ol class="inline-flex p-0 list-none">
                         <li class="flex items-center">
                             <a href="{{ route('pizze.index') }}"
-                               class="font-semibold leading-relaxed text-gray600"> {{ __('Utenti') }}</a>
+                               class="font-semibold leading-relaxed text-gray600">Pizza</a>
                         </li>
-                        @isset($user)
+                        @isset($pizza)
                             <li class="flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+                                <svg class="w-3 h-3 mx-3 text-gray-600 fillcurrent" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                    <path d="M285.476 272.971L91.132 467.314c9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-
+24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-
+33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475
+239.03c9.373 9.372 9.373 24.568.001 33.941z"/>
                                 </svg>
-                                <span class="font-semibold leading-relaxed textblack ">{{$user->name}}</span>
+                                <span class="font-semibold leading-relaxed textblack ">{{$pizza->name}}</span>
 
                             </li>
                         @else
@@ -23,7 +26,7 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
                                 </svg>
-                                <span class="font-semibold leading-relaxed textgray-600"> {{ __('Nuovo Utente') }}
+                                <span class="font-semibold leading-relaxed textgray-600">Nuova pizza
  </span>
                             </li>
                         @endisset
@@ -48,20 +51,17 @@ focus:border-blue-300 active:text-gray-800 active:bg-gray-50">
     </div>
     <div class="p-6">
         <div class="mb-5 -mt-1 text-center">
-            <div class="relative w-32 h-32 mx-auto mb-2 bg-gray-100 border
+            <div class="relative h-64 max-w-xs mx-auto mb-6 bg-gray-100 border
 rounded-full">
-                <img alt="" id="cover" class="object-cover w-full h-32
-rounded-full" src="@isset($user->avatar)/{{$user->avatar}} @else
-                    https://eu.ui-avatars.com/api/?name={{isset($user->name) ? $user->name :
-'Nuovo Utente' }} @endisset">
+                <img alt="" id="cover" class="object-cover w-full
+h-64 rounded-lg" src="@isset($pizza->featured_image)/{{$pizza->featured_image}} @else https://via.placeholder.com/256 @endisset">
             </div>
             <label for="uploadcover" class="items-center justify-between px-4
 py-2 font-medium text-left text-gray-600 bg-white border rounded-lg
 shadow-sm cursor-pointer inine-flex focus:outline-none hover:bg-gray-100">
                 <svg xmlns="http://www.w3.org/2000/svg" class="inline-flex
 flex-shrink-0 w-6 h-6 mr-1 -mt-1"
-                     viewBox="0 0 24 24" stroke-width="2"
-                     stroke="currentColor" fill="none"
+                     viewBox="0 0 24 24" stroke-width="2"stroke="currentColor" fill="none"
                      stroke-linecap="round" stroke-linejoin="round">
                     <rect x="0" y="0" width="24" height="24" stroke="none">
                     </rect>
@@ -71,9 +71,9 @@ flex-shrink-0 w-6 h-6 mr-1 -mt-1"
 2 0 0 1 2 -2"></path>
                     <circle cx="12" cy="13" r="3"></circle>
                 </svg>
-                {{ __('Carica Avatar') }}
+                {{ __('Immagine in Evidenza') }}
             </label>
-            <input name="avatar" id="uploadcover" accept="image/*"
+            <input name="featured_image" id="uploadcover" accept="image/*"
                    onchange="document.getElementById('cover').src =
 window.URL.createObjectURL(this.files[0])"
                    class="hidden" type="file">
@@ -85,7 +85,7 @@ window.URL.createObjectURL(this.files[0])"
         </div>
         <div class="mb-6 md:flex md:items-center">
             <div class="md:w-1/3">
-                <x-label class="px-1 text-sm text-gray-600 md:flex md:justifyend md:px-5" for="name" :value="__('Username')" />
+                <x-label class="px-1 text-sm text-gray-600 md:flex md:justifyend md:px-5" for="name" :value="__('nome')" />
             </div>
             <div class="md:w-2/3">
                 <div>
@@ -93,39 +93,37 @@ window.URL.createObjectURL(this.files[0])"
                              id="name"
                              class="block w-full md:w-3/5 "
                              name="name"
-                             :value="$user->name ?? old('name')"
+                             :value="$pizza->name ?? old('name')"
                              autofocus />
                 </div>
             </div>
         </div>
         <div class="mb-6 md:flex md:items-center">
             <div class="md:w-1/3">
-                <x-label class="px-1 text-sm text-gray-600 md:flex md:justifyend md:px-5" for="email" :value="__('Email')" />
+                <x-label class="px-1 text-sm text-gray-600 md:flex md:justifyend md:px-5" for="name" :value="__('prezzo')" />
             </div>
             <div class="md:w-2/3">
                 <div>
-                    <x-input type="email"
-                             id="email"
-                             class="block w-full md:w-3/5"
-                             name="email"
-                             :value="$user->email ?? old('email')"
+                    <x-input type="number"
+                             step=0.10
+                             id="price"
+                             class="block w-full md:w-3/5 "
+                             name="price"
+                             :value="$pizza->price ?? old('price')"
                              autofocus />
                 </div>
             </div>
         </div>
         <div class="mb-6 md:flex md:items-center">
             <div class="md:w-1/3">
-                <x-label class="px-1 text-sm text-gray-600 md:flex md:justify-
-end md:px-5" for="password" :value="__('Password')" />
+                <x-label class="px-1 text-sm text-gray-600 md:flex md:justifyend md:px-5" for="ingrediants" :value="__('ingredienti')" />
             </div>
             <div class="md:w-2/3">
                 <div>
-                    <x-input type="password"
-                             id="password"
-                             class="block w-full md:w-3/5"
-                             name="password"
-                             :value="old('password')"
-                             autofocus />
+ <textarea class="block w-full border-gray-300 rounded-md
+shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200
+focus:ring-opacity-50 md:w-3/5" name="ingrediants" id="ingrediants">{{$pizza->ingrediants ??
+old('ingrediants')}}</textarea>
                 </div>
             </div>
         </div>
