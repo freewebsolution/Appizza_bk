7855435dd6681b45188bdd1c76fe4511efde1886
lucio Ticali <info@freewebsolution.it>
2021-08-14 11:20:20 +0200

    Dinamicizzare la lsta prezzo

--- resources/views/frontend/sections/menu.blade.php
+++ resources/views/frontend/sections/menu.blade.php
@@ -26,7 +26,7 @@
                                 </div>
                             </div>
                         </div>
-                        @elseIf($loop->index === 3 || $loop->index === 4 || $loop->index === 5)
+                    @elseIf($loop->index === 3 || $loop->index === 4 || $loop->index === 5)
                         <div class="col-lg-4 d-flex ftco-animate">
                             <div class="services-wrap d-flex">
                                 <div class="text p-4">
@@ -56,105 +56,40 @@
             </div>
             <div class="row">
                 <div class="col-md-6">
-                    <div class="pricing-entry d-flex ftco-animate">
-                        <div class="img" style="background-image: url({{url('img/bg/pizza-1.jpg')}})"></div>
-                        <div class="desc pl-3">
-                            <div class="d-flex text align-items-center">
-                                <h3><span>Italian Pizza</span></h3>
-                                <span class="price">$20.00</span>
+                    @foreach($data['pizzaList'] as $pizza)
+                        @if($loop->index <= ($data['counter']) -1)
+                            <div class="pricing-entry d-flex ftco-animate">
+                                <div class="img" style="background-image: url({{$pizza->featured_image}})"></div>
+                                <div class="desc pl-3">
+                                    <div class="d-flex text align-items-center">
+                                        <h3><span>{{$pizza->name}}</span></h3>
+                                        <span class="price">{{$pizza->price}} €</span>
+                                    </div>
+                                    <div class="d-block">
+                                        <p>{{$pizza->ingrediants}}</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="d-block">
-                                <p>A small river named Duden flows by their place and supplies</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pricing-entry d-flex ftco-animate">
-                        <div class="img" style="background-image: url({{url('img/bg/pizza-2.jpg')}})"></div>
-                        <div class="desc pl-3">
-                            <div class="d-flex text align-items-center">
-                                <h3><span>Hawaiian Pizza</span></h3>
-                                <span class="price">$29.00</span>
-                            </div>
-                            <div class="d-block">
-                                <p>A small river named Duden flows by their place and supplies</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pricing-entry d-flex ftco-animate">
-                        <div class="img" style="background-image: url({{url('img/bg/pizza-3.jpg')}})"></div>
-                        <div class="desc pl-3">
-                            <div class="d-flex text align-items-center">
-                                <h3><span>Greek Pizza</span></h3>
-                                <span class="price">$20.00</span>
-                            </div>
-                            <div class="d-block">
-                                <p>A small river named Duden flows by their place and supplies</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pricing-entry d-flex ftco-animate">
-                        <div class="img" style="background-image: url({{url('img/bg/pizza-4.jpg')}})"></div>
-                        <div class="desc pl-3">
-                            <div class="d-flex text align-items-center">
-                                <h3><span>Bacon Crispy Thins</span></h3>
-                                <span class="price">$20.00</span>
-                            </div>
-                            <div class="d-block">
-                                <p>A small river named Duden flows by their place and supplies</p>
-                            </div>
-                        </div>
-                    </div>
+                        @endif
+                    @endforeach
                 </div>
-
                 <div class="col-md-6">
-                    <div class="pricing-entry d-flex ftco-animate">
-                        <div class="img" style="background-image: url({{url('img/bg/pizza-5.jpg')}})"></div>
-                        <div class="desc pl-3">
-                            <div class="d-flex text align-items-center">
-                                <h3><span>Hawaiian Special</span></h3>
-                                <span class="price">$49.91</span>
+                    @foreach($data['pizzaList'] as $pizza)
+                        @if($loop->index >= ($data['counter']))
+                            <div class="pricing-entry d-flex ftco-animate">
+                                <div class="img" style="background-image: url({{$pizza->featured_image}})"></div>
+                                <div class="desc pl-3">
+                                    <div class="d-flex text align-items-center">
+                                        <h3><span>{{$pizza->name}}</span></h3>
+                                        <span class="price">{{$pizza->price}} €</span>
+                                    </div>
+                                    <div class="d-block">
+                                        <p>{{$pizza->ingrediants}}</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="d-block">
-                                <p>A small river named Duden flows by their place and supplies</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pricing-entry d-flex ftco-animate">
-                        <div class="img" style="background-image: url({{url('img/bg/pizza-6.jpg')}})"></div>
-                        <div class="desc pl-3">
-                            <div class="d-flex text align-items-center">
-                                <h3><span>Ultimate Overload</span></h3>
-                                <span class="price">$20.00</span>
-                            </div>
-                            <div class="d-block">
-                                <p>A small river named Duden flows by their place and supplies</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pricing-entry d-flex ftco-animate">
-                        <div class="img" style="background-image: {{asset('img/bg/pizza-7.jpg')}}"></div>
-                        <div class="desc pl-3">
-                            <div class="d-flex text align-items-center">
-                                <h3><span>Bacon Pizza</span></h3>
-                                <span class="price">$20.00</span>
-                            </div>
-                            <div class="d-block">
-                                <p>A small river named Duden flows by their place and supplies</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pricing-entry d-flex ftco-animate">
-                        <div class="img" style="background-image: url({{url('img/bg/pizza-8.jpg')}})"></div>
-                        <div class="desc pl-3">
-                            <div class="d-flex text align-items-center">
-                                <h3><span>Ham &amp; Pineapple</span></h3>
-                                <span class="price">$20.00</span>
-                            </div>
-                            <div class="d-block">
-                                <p>A small river named Duden flows by their place and supplies</p>
-                            </div>
-                        </div>
-                    </div>
+                        @endif
+                    @endforeach
                 </div>
             </div>
         </div>
